182e7f0ed0cfb6ca23a9c742492173868d78ceca
Siyanka Fiodar <siyankafiodar@gmail.com>
2022-05-25 01:50:32 +0300

    main

---
+++ Task_JS_4/Script/script.js
@@ -0,0 +1,48 @@
+// Task_1
+
+
+// function isMozartHere(str) {
+//     let lowerCase = str.toLowerCase();
+    
+//     return lowerCase.includes("mozart");
+// }
+
+// Task_2
+
+// function showUppercaseFirstLetter(str) {
+//     if (!str) return str;
+
+//     return str[0].toUpperCase() + str.slice(1);
+// }
+
+// Task_3
+
+// let harryPotterTeam = ["Albus", "Hermione", "Ronald", "Draco", "Neville"];
+
+// let teamA = harryPotterTeam.filter((item) => {
+// return item.includes("a");
+
+// })
+// let teamH = harryPotterTeam.filter((item) => {
+//     return item.includes("H");
+// })
+// console.log(harryPotterTeam);
+// console.log(teamA);
+// console.log(teamH);
+
+// Task_4
+
+let arr = [6, 3, "ten", 1, true, "4"];
+
+let sumArr = [];
+let multiplicationArr = [];
+arr.forEach((item) => {
+    sumArr.map((item) => item).reduce((item, 0));
+
+})
+console.log(sumArr);
+// arr.forEach((item) => {
+//     multiplicationArr.map(item => item * 2);
+// })
+
+// console.log(multiplicationArr);
